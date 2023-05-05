656463752d14829cb8214a90385eb819301edb01
gonzalezlg <gonzalez.lucas.91@gmail.com>
2023-05-05 20:04:23 -0300

    Se modifica estilos en seccion Home

---
+++ asset/js/home.js
@@ -0,0 +1,8 @@
+// Script para poder ralizar efecto en el texto del home
+
+var typed = new Typed(".auto-type", {
+    strings: ["Software", " Software Developer"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+});
